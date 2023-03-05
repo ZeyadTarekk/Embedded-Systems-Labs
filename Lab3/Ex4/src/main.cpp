1cf830db222b2b1cc99935952fd62772bf1f573d
Zeyad Tarek <zeyad.ta01@gmail.com>
2023-03-05 13:48:16 +0200

    Ex4

--- Lab3/Ex4/src/main.cpp
+++ Lab3/Ex4/src/main.cpp
@@ -1,9 +1,41 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+// initialize timer, interrupt and variable
+void timer1_init()
+{
+  TCNT2 = 0;
+  OCR2A = 250;
 
-void setup() {
-  // put your setup code here, to run once:
+  DDRB |= 8;
+
+  // non PWM
+  TCCR2B |= (1 << 7);
+
+  // CTC mode
+  TCCR2B &= ~8;
+  TCCR2A |= 2;
+  TCCR2A &= ~1;
+
+  // toggle OC2A
+  TCCR2A |= (1 << 6);
+  TCCR2A &= ~(1 << 7);
+
+  // setting clock/1024
+  TCCR2B |= 7;
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+int main()
+{
+  // connect led to pin PB1
+  // DDRB |= (1 << PB0);
+  // DDRD |= (1 << PD5);
+  // initialize timer
+  timer1_init();
+  while (1)
+  {
+    // do nothing
+    // whenever a match occurs
+    // OC1A is toggled automatically!
+    // no need to keep track of any flag bits or ISR
+  }
+  return 0;
 }
